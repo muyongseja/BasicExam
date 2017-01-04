26e67dded2464a2011d677b72c92657fe1d4abbe
Jeong Woo <netsong7@naver.com>
2017-01-04 14:39:36 +0900

    test

--- src/main/java/basic08/Sample.java
+++ src/main/java/basic08/Sample.java
@@ -3,6 +3,7 @@ package basic08;
 import org.springframework.beans.factory.annotation.Value;
 
 public class Sample {
+	/*
 	@Value("#{config.environment}")
 	private String environment;
 	@Value("#{config.getCountry()}")
@@ -12,6 +13,29 @@ public class Sample {
 	@Value("#{config.splitName('John F Kenedy')}")
 	private String[] splitName;
 	
+	public void display(){
+		System.out.println(environment + " : " + country
+			+ " : " + state + " : " + splitName[0]);
+	}
+	*/
+	
+	private String environment;
+	private String country;
+	private String state;
+	private String[] splitName;
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setSplitName(String[] splitName) {
+		this.splitName = splitName;
+	}
+
 	public void display(){
 		System.out.println(environment + " : " + country
 			+ " : " + state + " : " + splitName[0]);
